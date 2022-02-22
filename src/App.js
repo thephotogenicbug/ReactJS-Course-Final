2f7344e6cd6d48c1c5ade7a35128da741e269baa
thephotogenicbug <navinkumarachari@gmail.com>
2022-02-22 16:00:50 +0530

    22-01-2022 : Install Bootstrap and React-router-dom

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import React from 'react';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './Components/Header/Header';
 import FirstComponent from './Components/ClassComponent/ClassComponent1';
 import SecondComponent from './Components/ClassComponent/ClassComponent2';
 import FunctionComponent from './Components/FunctionalComponent/FunctionalComponent';
-import './App.css';
 import EmployeeParent from './Components/Props/FunctionProps/EmployeeParent';
 import ClassEmployeeParent from './Components/Props/ClassProps/EmployeeParent';
+
 
 function App() {
   return (
     <div className="App">
-      <FirstComponent />
-      <SecondComponent />
-      <hr />
-      <FunctionComponent />
-      <hr />
-      <EmployeeParent />
-      <hr />
-      <ClassEmployeeParent />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<FirstComponent />} />
+          <Route exact path="/class2" element={<SecondComponent />} />
+          <Route exact path="/classprops" element={<ClassEmployeeParent />} />
+          <Route exact path="/function" element={<FunctionComponent />} />
+          <Route exact path="/functionprops" element={<EmployeeParent />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
